a12b408f2144c1c7ded43ef56340a9514e21e148
unknown <micha454@umn.edu>
2013-12-10 13:24:39 -0600

    Stubbed out TranscriptHandler

--- src/TranscriptHandler.java
+++ src/TranscriptHandler.java
@@ -1,4 +1,42 @@
+import com.google.gson.JsonObject;
+
+import edu.umn.csci5801.model.StudentRecord;
+
 
 public class TranscriptHandler {
-
+	DataManager dbManager;
+	
+	public TranscriptHandler(DataManager dbMan){
+		dbManager = dbMan;
+	}
+	
+	public StudentRecord getStudentRecord(String studentID){
+		StudentRecord record = new StudentRecord();
+		
+		// TODO: stuff
+		
+		return null;
+	}
+	
+	public void updateTranscript(String studentID, StudentRecord record){
+		// TODO: stuff
+	}
+	
+	public void addNote(String studentID, String note){
+		// TODO: stuff
+	}
+	
+	private boolean validateTranscript(StudentRecord record){
+		// TODO: stuff
+		
+		return true;
+	}
+	
+	private JsonObject recordToJson(StudentRecord record){
+		JsonObject retVal = new JsonObject();
+		
+		// TODO: stuff
+		
+		return null;
+	}
 }
