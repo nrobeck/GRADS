5ae2969e9e07b83301080967c2cffc028867687f
Nathan Robeck <robec009@umn.edu>
2013-12-11 01:36:13 -0600

    fixed up the structure a bit (tests now in their own package inside source) and made JUnit a library.

---
+++ src/testing/GRADSTests.java
@@ -0,0 +1,21 @@
+package testing;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.ComparisonFailure;
+import org.junit.FixMethodOrder;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+
+
+
+public class GRADSTests {
+    
+
+}
