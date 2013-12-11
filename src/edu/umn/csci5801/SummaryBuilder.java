40218fede5e95029a33578a7124b68b3d178e131
Nathan Robeck <robec009@umn.edu>
2013-12-11 13:38:40 -0600

    Summary Builder: added more inline comments, modified some javadocs slightly, restructured imports to be explicit (per assignment instructions), added getStudentSummary method.

--- src/edu/umn/csci5801/SummaryBuilder.java
+++ src/edu/umn/csci5801/SummaryBuilder.java
@@ -2,7 +2,14 @@ package edu.umn.csci5801;
 // SummaryBuilder.java
 //
 
-import edu.umn.csci5801.model.*;
+import edu.umn.csci5801.model.CheckResultDetails;
+import edu.umn.csci5801.model.CourseTaken;
+import edu.umn.csci5801.model.Grade;
+import edu.umn.csci5801.model.MilestoneSet;
+import edu.umn.csci5801.model.ProgressSummary;
+import edu.umn.csci5801.model.RequirementCheckResult;
+import edu.umn.csci5801.model.StudentRecord;
+
 
 import java.lang.String;
 import java.sql.DatabaseMetaData;
@@ -19,7 +26,7 @@ import com.google.gson.JsonObject;
 
 /**
  *
- * Module of GRADS used to create student summary
+ * Module of GRADS used to create student summary.
  * @author Kyle
  *
  */
@@ -35,16 +42,20 @@ public class SummaryBuilder {
     }
 
     /**
-     * Creates a graduate progress summary based on the student's transcript
+     * Creates a graduate progress summary based on the student's transcript and courses to simulate.
      * @param studentID The id of the student
      * @param simCourses A list of courses to simulate
      * @return A completed summary of a student's progress towards his/her graduate degree
      */
     public ProgressSummary createStudentSummary(String studentID, List<CourseTaken> simCourses) {
+        //initialize data objects
         ProgressSummary summary = new ProgressSummary();
-        StudentRecord record = new StudentRecord();	// TODO get this from transcript handler
+        //get the student record from transcriptHandler
+        StudentRecord record = new StudentRecord();
+        record = transcriptHandler.getStudentRecord(studentID);
         JsonObject plan = new JsonObject();
 
+        //set data for the summary
         summary.setStudent(record.getStudent());
         summary.setDepartment(record.getDepartment());
         summary.setDegreeSought(record.getDegreeSought());
@@ -53,34 +64,39 @@ public class SummaryBuilder {
         summary.setCommittee(record.getCommittee());
         summary.setNotes(record.getNotes());
 
+        //get the courses the student has taken from their record
         List<CourseTaken> courses = record.getCoursesTaken();
 
+        //check the requirements and set results
         summary.setRequirementCheckResults(checkPlanRequirements(plan, courses, record.getMilestonesSet()));
 
-        return null;
+        return summary;
     }
 
     /**
-     * Creates a list of RequirementCheckResults for the summary
+     * Creates a list of RequirementCheckResults for the summary.
      * @param plan - the plan that will be checked
      * @param courses - the courses that the student has taken
      * @param milestones - a list of milestones of the student
      * @return a list of Requirement Check Results based on the plan/student progress
      */
     private List<RequirementCheckResult> checkPlanRequirements(JsonObject plan, List<CourseTaken> courses, List<MilestoneSet> milestones) {
-        String[] requirements = new String[1];
+        //create the needed elements
         JsonElement reqs;
         List<RequirementCheckResult> retVal = new ArrayList<RequirementCheckResult>() {
         };
-        /*
+
+        /* TODO?
         if (plan.get("requirements") == null || !plan.get("requirements").getClass().equals()){
             // throw a error, no requirements
             return null;
         }
         */
 
+        //get the requirements for the plan
         reqs = plan.get("requirements");
 
+        //collect the requirements as strings
         for( JsonElement e : reqs.getAsJsonArray()){
             JsonObject req = e.getAsJsonObject();
             String name = req.get("name").getAsString();
@@ -134,7 +150,7 @@ public class SummaryBuilder {
 
             // check Breadth requirements
             else if(req.get("type").getAsString().equals("breadth")){
-
+                //TODO
             }
 
             else {
@@ -148,31 +164,45 @@ public class SummaryBuilder {
         return null;
     }
 
-
     /**
-     * calculate the GPA of a list of courses
+     * Calculate the GPA of a list of courses taken.
      * @param courses - the list of courses to derive a GPA from
      * @return the GPA
      */
     private double calculateGPA(List<CourseTaken> courses) {
+        //initialize parameters
         int credits = 0;
         int sum = 0;
         int temp;
 
+        //loop through each course taken
         for (CourseTaken c: courses){
             // check if the course is not a S/N or a sim course
             if(c.getGrade().ordinal() <= Grade.F.ordinal() ){
+                //get the credits the course was worth
                 temp = Integer.parseInt(c.getCourse().getNumCredits());
+                //add the credits to the running total
                 credits += temp;
+                //add the gpa integer value for the credits to the sum
                 sum += temp * c.getGrade().numericValue();
             }
         }
 
+        //divide sum of gpa totals by number fo credits taken to get gpa and then return it
         return sum/credits;
     }
 
+    /**
+     * Return the progress summary based solely upon a student's transcript and no simulated courses.
+     * Calls createStudentSummary with null for simulated courses
+     * @see createStudentSummary method used to perform all creation
+     * @param userId user id of the student whose progress summary is to be generated.
+     * @return progress summary for the student.
+     */
     public ProgressSummary getStudentSummary(String userId) {
-        // TODO Auto-generated method stub
-        return null;
+        //create the progress summary
+        ProgressSummary progress = createStudentSummary(userId, null);
+        //return the progress summary
+        return progress;
     }
 }
