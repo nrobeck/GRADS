ff0ffb5849cc3f8f82d91b6ce16f586fc79c3206
Nathan Robeck <robec009@umn.edu>
2013-12-12 12:38:29 -0600

    started testing for requirement 1

--- src/testing/InformationRetrievalTests.java
+++ src/testing/InformationRetrievalTests.java
@@ -18,8 +18,31 @@ import edu.umn.csci5801.DataManager;
 import edu.umn.csci5801.SummaryBuilder;
 import edu.umn.csci5801.TranscriptHandler;
 import edu.umn.csci5801.model.Department;
+import edu.umn.csci5801.model.StudentRecord;
 
 //tests for requirement 1
 public class InformationRetrievalTests {
-    
+    //variables for testing
+    GRADS testGrads = new GRADS("resources/studentsTest.txt", "resources/courses.txt", "resources/usersTest.txt");
+    StudentRecord testRecord = new StudentRecord();
+
+
+    //test 1.1
+    public void retrieveAStudentTranscriptTest() {
+        try {
+            testGrads.setUser("tolas9999");
+        } catch (Exception e) {
+            System.out.println("X500 invalid");
+            e.printStackTrace();
+        }
+        try {
+            testRecord = testGrads.getTranscript("nguy0621");
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        Assert.assertEquals(testRecord.getStudent().getId() , "nguy0621");
+    }
+
+
 }
