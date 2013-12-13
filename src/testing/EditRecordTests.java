66498565329298e43d4d94968ad043e87f42144b
Kyle <micha454@umn.edu>
2013-12-12 19:21:01 -0600

    Added premission tests.

--- src/testing/EditRecordTests.java
+++ src/testing/EditRecordTests.java
@@ -13,8 +13,77 @@ import org.junit.FixMethodOrder;
 import org.junit.Ignore;
 import org.junit.Rule;
 
+import edu.umn.csci5801.GRADS;
+import edu.umn.csci5801.model.StudentRecord;
+import exceptions.UserNotGPCException;
+
 
 //tests for requirement 6
+/**
+ * 
+ * @author Kyle
+ *
+ */
 public class EditRecordTests {
+	GRADS grads = new GRADS("src/resources/studentsTest001.txt", "src/resources/courses.txt", "src/resources/usersTest.txt");
+	
+	/**
+	 * Test if a student is rejected from updating a transcript
+	 */
+	@Test
+	public void studentAttempt(){
+		
+		try {
+			grads.setUser("studentPHD");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		boolean passed = false;
+		
+		StudentRecord record = new StudentRecord();
+		
+		try {
+			grads.updateTranscript("studentPHD", record);
+		} catch (UserNotGPCException e) {
+			passed = true;
+		}  catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Assert.assertTrue("A Student attempting to update a transcipt must cause a UserNotGPCException", passed);
+	}
+	
+	/**
+	 * Test if a gpc can update a transcript
+	 */
+	@Test
+	public void gpcAttempt(){
+		
+		try {
+			grads.setUser("gpc001");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		boolean passed = true;
+		
+		StudentRecord record;
+		try {
+			record = grads.getTranscript("studentPHD");
+			try {
+				grads.updateTranscript("studentPHD", record);
+			}  catch (Exception e) {
+				passed = false;
+			}
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 
+		Assert.assertTrue("A GPC must be allowed to update the transcript", passed);
+	}
 }
